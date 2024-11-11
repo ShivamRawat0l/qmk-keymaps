81b60cfaffb3dd92ec0b66878371467b4de51297
Shivam Rawat <shivamrawat6670@gmail.com>
2024-11-11 14:14:16 +0530

    feat: Updated the firmware to colemak

--- sofle_choc/keymap.c
+++ sofle_choc/keymap.c
@@ -15,52 +15,53 @@
  */
 #include QMK_KEYBOARD_H
 
-#define _WIN  0
-#define _MAC  1
-#define _SPCL  2
+ 
+#define _MAC  0
+#define _SPCL  1
+#define _NUM  2
+
 
 enum custom_keycodes {
   VIM_CMD = SAFE_RANGE,
-  VIM_QUIT,
   VIM_SAVE
 };
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /*
- * QWERTY
+ * Colemak
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * |  `   |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  | MODE |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | ESC  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |  \   |
+ * | ESC  |   !  |   @  |   #  |   $  |  %   |                    |   ^  |   &  |   *  |   (  |   )  |  \   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | Tab  |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  | Enter|
+ * | Tab  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  | Enter|
  * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
  * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RShift|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *            |      | LGUI | CTRL | Space| / LALT  /       \ CTRL \  | Bspc | SPCL |      |      |
+ *            |      |      |      | ESCAPE|/  Save /       \      \  |      |      |      |      |
  *            |      |      |      |      |/       /         \      \ |      |      |      |      |
  *            `----------------------------------'           '------''---------------------------'
  */
 
-[_WIN] = LAYOUT(
-    KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                              KC_6,     KC_7,     KC_8,    KC_9,    KC_0,    DF(_MAC),
-    KC_ESC,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                              KC_Y,     KC_U,     KC_I,    KC_O,    KC_P,    KC_BSLS,
-    KC_TAB,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                              KC_H,     KC_J,     KC_K,    KC_L,    KC_SCLN, KC_ENT,
-    KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    _______,       _______,   KC_N,     KC_M,     KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                      _______, KC_LGUI, KC_LCTL, KC_SPC,  KC_LALT,       KC_LCTL,   KC_BSPC,  MO(_SPCL),KC_DEL,  _______
+[_NUM] = LAYOUT(
+    KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                              KC_6,     KC_7,     KC_8,    KC_9,    KC_0,    KC_CAPS,
+    KC_ESC,  KC_EXLM,  KC_AT, KC_HASH, KC_DLR,   KC_PERC,                           KC_CIRC,     KC_AMPR,     KC_ASTR,    KC_LPRN,    KC_RPRN, KC_BSLS,
+    KC_TAB,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                              KC_6,     KC_7,     KC_8,    KC_9,    KC_0,    KC_ENT,
+    KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_D,    KC_V,    _______,       _______,   KC_K,     KC_H,     KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+                       _______, _______, _______, VIM_CMD, VIM_SAVE,    _______, _______,  _______,  _______, _______
 ),
 
 /*
- * QWERTY
+ * Colemak
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * |  `   |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  | MODE |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | ESC  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |  \   |
+ * | ESC  |   Q  |   W  |   F  |   P  |   B  |                    |   J  |   L  |   U  |   Y  |   ;  |  \   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | Tab  |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  | Enter|
+ * | Tab  |   A  |   R  |   S  |   T  |   G  |-------.    ,-------|   M  |   N  |   E  |   I  |   O  | Enter|
  * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
- * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RShift|
+ * |LShift|   Z  |   X  |   C  |   D  |   V  |-------|    |-------|   K  |   H  |   ,  |   .  |   /  |RShift|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *            |      | LGUI | CMD  | Space| / LALT  /       \ CTRL \  | Bspc | SPCL |  DEL |      |
  *            |      |      |      |      |/       /         \      \ |      |      |      |      |
@@ -68,15 +69,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_MAC] = LAYOUT(
-    KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                              KC_6,     KC_7,     KC_8,    KC_9,    KC_0,    DF(_WIN),
-    KC_ESC,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                              KC_Y,     KC_U,     KC_I,    KC_O,    KC_P,    KC_BSLS,
-    KC_TAB,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                              KC_H,     KC_J,     KC_K,    KC_L,    KC_SCLN, KC_ENT,
-    KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    _______,       _______,   KC_N,     KC_M,     KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                      _______, KC_LGUI, KC_LCMD, KC_SPC,  KC_LALT,       KC_LCTL,   KC_BSPC,  MO(_SPCL),KC_DEL,  _______
+    KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                              KC_6,     KC_7,     KC_8,    KC_9,    KC_0,    KC_CAPS,
+    KC_ESC,   KC_Q,   KC_W,    KC_F,    KC_P,    KC_B,                              KC_J,     KC_L,     KC_U,    KC_Y,    KC_SCLN, KC_BSLS,
+    KC_TAB,   KC_A,   KC_R,    KC_S,    KC_T,    KC_G,                              KC_M,     KC_N,     KC_E,    KC_I,    KC_O,    KC_ENT,
+    KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_D,    KC_V,    _______,       _______,   KC_K,     KC_H,     KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+                      KC_LGUI, MO(_NUM), KC_LCMD, KC_SPC,  KC_LALT,       KC_LCTL,   KC_BSPC,  MO(_SPCL),KC_DEL,  _______
 ),
 
 /*
- * QWERTY
+ * Symbols 
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * |  F11 |  F1  |  F2  |  F3  |  F4  |  F5  |                    |  F6  |  F7  |  F8  |  F9  |  F10 | F12  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
@@ -86,7 +87,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
  * | VOL- |   -  |  [   |      |   ]  |  +   |-------|    |-------|      |      |      |      |      |      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *            |      |      |      |      | /       /       \      \  |      |      |      |      |
+ *            |      |      |      | ESCAPE|/  Save /       \      \  |      |      |      |      |
  *            |      |      |      |      |/       /         \      \ |      |      |      |      |
  *            `----------------------------------'           '------''---------------------------'
  */
@@ -94,9 +95,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_SPCL] = LAYOUT(
     KC_F11,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                        KC_F6,    KC_F7,    KC_F8,   KC_F9,    KC_F10,  KC_F12,
     KC_MUTE,  _______, KC_LPRN, KC_EQUAL,KC_RPRN, _______,                      KC_EXLM,  KC_AT,    KC_HASH, KC_DLR,   KC_PERC, KC_AMPR,
-    KC_VOLU,  KC_QUOT, KC_LCBR, KC_UNDS, KC_RCBR, KC_DQT,                       KC_LEFT,  KC_DOWN,  KC_UP,   KC_RIGHT, VIM_CMD,VIM_QUIT,
+    KC_VOLU,  KC_QUOT, KC_LCBR, KC_UNDS, KC_RCBR, KC_DQT,                       KC_LEFT,  KC_DOWN,  KC_UP,   KC_RIGHT, _______, _______,
     KC_VOLD,  KC_MINS, KC_LBRC, _______, KC_RBRC, KC_PLUS, _______,    _______, _______,  _______,  _______, _______,  _______, _______,
-                       _______, _______, _______, _______, VIM_SAVE,    _______, _______,  _______,  _______, _______
+                       _______, _______, _______, VIM_CMD, VIM_SAVE,    _______, _______,  _______,  _______, _______
 )
 
 };
@@ -114,22 +115,13 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case VIM_CMD:
         if (record->event.pressed) {
           tap_code16(LCTL(KC_C));
-          tap_code16(KC_COLN);
-          return 0;
+           return 0;
         } 
     case VIM_SAVE:
         if (record->event.pressed) {
-          tap_code16(LCTL(KC_C));
-          send_string(":wa!\n");
+          tap_code16(LCTL(KC_S));
           return 0;
         } 
-    case VIM_QUIT:
-        if (record->event.pressed) {
-          tap_code16(LCTL(KC_C));
-          send_string(":wqa!\n");
-          return 0;
-        } 
-    }
     return 1;
 }
 
